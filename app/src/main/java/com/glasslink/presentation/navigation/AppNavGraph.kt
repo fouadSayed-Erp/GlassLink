9f0d1487e79aef77b16c8c034ca0ba57468b1a17
fouadSayed-Erp <fouad@gmail.com>
2026-09-06 01:17:11 +0300

    fix: v1.5 make NearbyScannerScreen accept Any and fix AppNavGraph call

--- app/src/main/java/com/glasslink/presentation/navigation/AppNavGraph.kt
+++ app/src/main/java/com/glasslink/presentation/navigation/AppNavGraph.kt
@@ -3,14 +3,14 @@ package com.glasslink.presentation.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.*
 import com.glasslink.presentation.chat.ChatListScreen
-import com.glasslink.presentation.scanner.NearbyScannerScreen
+                NearbyScannerScreen()
 import com.glasslink.presentation.vault.MediaVaultScreen
 sealed class Screen(val route: String){ object Scanner: Screen("scanner"); object Chats: Screen("chats"); object Vault: Screen("vault") }
 @Composable
 fun AppNavGraph(peers: List<com.glasslink.domain.model.Peer>) {
     val nav = rememberNavController()
     NavHost(nav, startDestination=Screen.Scanner.route) {
-        composable(Screen.Scanner.route){ NearbyScannerScreen(peers,{}) }
+                NearbyScannerScreen()
         composable(Screen.Chats.route){ ChatListScreen(peers,{}) }
         composable(Screen.Vault.route){ MediaVaultScreen() }
     }
